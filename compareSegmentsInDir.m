12a4317cda971809df96c8ae020d06f5b5013656
Thanuja Ambegoda <thanujadax@gmail.com>
2016-12-15 14:30:37 +0100

    ted plot

--- compareSegmentsInDir.m
+++ compareSegmentsInDir.m
@@ -1,14 +1,14 @@
 function compareSegmentsInDir()
 
 % the images should be index images (1...K)
-groundTruthDir = '';
+groundTruthDir = '/home/thanuja/DATA/ISBI2012/gala/flatfiles/test/gt-index';
 % groundTruthImg = '/home/thanuja/DATA/ISBI2012/gala/flatfiles/test/probMaps_rfc/020.tif';
-reconstructionsDir = '/home/thanuja/RESULTS/isbi2012/graphcuts/rfc/tryk';
+reconstructionsDir = '/home/thanuja/RESULTS/isbi2012/rfc_ilp_cnn/test_21_30_bwidx';
 
 gtType = 'tif';
 rType = 'png';
 
-outputFile = '/home/thanuja/RESULTS/isbi2012/graphcuts/rfc/tryk/evaluation.txt';
+outputFile = '/home/thanuja/RESULTS/isbi2012/rfc_ilp_cnn/test_21_30_bwidx/eval.txt';
 
 gtFileList = dir(fullfile(groundTruthDir,strcat('*.',gtType)));
 rFileList = dir(fullfile(reconstructionsDir,strcat('*.',rType)));
@@ -25,7 +25,7 @@ else
     vi_n = zeros(numImg,1);
     numSegments = zeros(numImg,1);
     fid = fopen(outputFile,'w');
-    fprintf(fid,'Normalized RI, GCE, VoI for each image per segment \n');
+    fprintf(fid,'RI, GCE, VoI for each image \n');
     for i=1:numImg
         gtLabels = imread(fullfile(groundTruthDir,gtFileList(i).name));
         % gtLabels = imread(groundTruthImg);
@@ -35,19 +35,19 @@ else
         gtBW = im2bw(gtLabels,0);
         cc = bwconncomp(gtBW);
         numSegments(i) = length(cc.PixelIdxList);
-        ri_n(i) = ri(i)/numSegments(i);
-        gce_n(i) = gce(i)/numSegments(i);
-        vi_n(i) = vi(i)/numSegments(i);
+        ri_n(i) = ri(i);
+        gce_n(i) = gce(i);
+        vi_n(i) = vi(i);
         fprintf(fid,'Image:%d , RI=%f, GCE=%f, VoI=%f, numSeg=%d\n',...
-            i,ri_n(i),gce_n(i),vi_n(i),numSegments(i));
+            i,ri_n(i),gce_n(i),vi_n(i));
     end
-    % get the avg scores (normalized per segment)
+    % get the avg scores over all images
     ri_n_av = mean(ri_n);
     gce_n_av = mean(gce_n);
     vi_n_av = mean(vi_n);
     
     fprintf(fid,'****************************************');
-    fprintf(fid,'Average: RI=%f, GCE=%f, VoI=%f',ri_n_av,gce_n_av,vi_n_av);
+    fprintf(fid,'Average per image: RI=%f, GCE=%f, VoI=%f',ri_n_av,gce_n_av,vi_n_av);
     
     
     fclose(fid);
--- tedEvaluation2D.m
+++ tedEvaluation2D.m
@@ -4,14 +4,17 @@
 % after creating the outputFile using this script, use readTedOutput to get
 % the per slice normalized error for all sections
 groundTruthDir = '/home/thanuja/RESULTS/cvpr/TED/groundtruth';
-reconstructionDir = '/home/thanuja/RESULTS/cvpr/TED/cnn';
+reconstructionDir = '/home/thanuja/RESULTS/cvpr/TED2/rfc_stc_cnn_stc';
 configurationFile = '/home/thanuja/projects/RESULTS/contours/ted/20161107_isbi000_indexed/ted.conf';
-outputFile = '/home/thanuja/RESULTS/cvpr/TED/cnn/output_30.txt';
-gtType = 'tif';
-rType = 'tif';
+outputFile = '/home/thanuja/RESULTS/cvpr/TED2/rfc_stc_cnn_stc/ted_output_30.txt';
+gtType = 'png';
+rType = 'png';
 
 % tedCommand = '/home/thanuja/projects/external/ted/build/binaries/ted';
 tedCommand = '/home/thanuja/projects/RESULTS/contours/ted/20161107_isbi000_indexed/ted';
+
+% groundTruthList = dir(fullfile(groundTruthDir,strcat('*.',gtType)));
+% reconstructionList = dir(fullfile(reconstructionDir,strcat('*.',rType)));
 
 groundTruthList = dir(groundTruthDir);
 reconstructionList = dir(reconstructionDir);
@@ -22,9 +25,15 @@ reconstructionList(1:2) = [];
 numGT = length(groundTruthList);
 numRec = length(reconstructionList);
 
-% if(numGT ~= numRec)
-%     error('Number of ground truth images and reconstruction images are not equal!');
-% else
+str1 = sprintf('Number of ground truth subdirs = %d',numGT);
+disp(str1)
+str1 = sprintf('Number of reconstruction subdirs = %d',numRec);
+disp(str1)
+
+
+if(numGT ~= numRec)
+    error('Number of ground truth images and reconstruction images are not equal!');
+else
     for i=1:numGT
     % write the full command
     unixCommand = sprintf(...
@@ -45,5 +54,5 @@ numRec = length(reconstructionList);
     fclose(fid);
     
     end
-% end
+end
     
